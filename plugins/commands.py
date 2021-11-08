da845963a8a1dc10c459b652e9b88fa6dd15a653
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2021-11-08 22:20:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,13 +72,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updat', url='https://t.me/EvaMariaUpdates')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+       
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
